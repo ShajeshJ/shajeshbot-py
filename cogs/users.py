eba9754d9d993981221dc5e838cd465dc9824333
SHAJESH JEGATHEESWARAN <shajesh.14@gmail.com>
2020-07-27 21:59:41 -0400

    Fix bug where messages are spam sent when name is too long

--- cogs/users.py
+++ cogs/users.py
@@ -9,20 +9,25 @@ class UsersCog(cmd.Cog, name='Users'):
     @cmd.Cog.listener(name='on_member_update')
     async def fix_nicknames(self, before, after):
         if after.nick:
+            new_nick = null
             identifier = f' [{after.name}]'
 
-            if not after.nick.endswith(identifier):
+            if before.name != after.name:
+                new_nick = null
+            elif not after.nick.endswith(identifier):
                 new_nick = f'{after.nick}{identifier}'
                 if len(new_nick) > 32:
                     await before.send(f'Your new nickname cannot be longer than {32-len(identifier)} characters')
                     new_nick = before.nick
+            else:
+                return
 
-                try:
-                    await after.edit(nick=new_nick)
-                except discord.errors.Forbidden:
-                    print(f'Could not change nickname "{after.nick}" for {after}')
-                except discord.errors.HTTPException as e:
-                    print(str(e))
+            try:
+                await after.edit(nick=new_nick)
+            except discord.errors.Forbidden:
+                print(f'Could not change nickname "{after.nick}" for {after}')
+            except discord.errors.HTTPException as e:
+                print(str(e))
 
 
 def setup(bot):
